35ad1add9f974a5b02c9319a59b0f7c3e5ff7535
unknown <ayushGautam.dev@gmail.com>
2023-11-21 19:02:11 +0530

    changed done

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 const db = process.env
 exports.connectDb =()=>{
-    mongoose.connect(`mongodb+srv://${db.DB_USERNAME}:${db.DB_PASSWORD}@portfolio.9t0vujh.mongodb.net/${db.DB_CLUSTER}?retryWrites=true&w=majority`).then(()=>{
+    mongoose.connect(`mongodb+srv://ayushgautam:JjIf0R1b0Q0mtXfL@portfolio.9t0vujh.mongodb.net/mails`).then(()=>{
         console.log('connected to database ');
     }).catch((err)=>{
         console.log(err.message);
